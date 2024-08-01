923c98b58232b4b23399eef2ca9774f98d0efc76
Riteshgaikwad <gaikwadritesh2003@gmail.com>
2024-08-01 16:09:46 +0530

    project completed

--- script.js
+++ script.js
@@ -17,16 +17,23 @@ let passwordLength = 10;
 let checkCount = 0;
 handleSlider();
 // set strength circle to grey
+setIndication("#ccc");
 
 //set passwordLength
 function handleSlider() {
   inputSlider.value = passwordLength;
   lengthDisplay.innerText = passwordLength;
+
+  const min = inputSlider.min;
+  const max = inputSlider.max;
+  inputSlider.style.backgroundSize =
+    ((passwordLength - min) * 100) / (max - min) + "% 100%";
 }
 
 function setIndication(color) {
   indicator.style.backgroundColor = color;
   //shadow
+  indicator.style.boxShadow = `0px 0px 12px 1px ${color}`;
 }
 
 function getRndInteger(min, max) {
@@ -83,118 +90,115 @@ async function copyContent() {
   //to make copy span visible
   copyMsg.classList.add("active");
 
-  setTimeout(()=>{
+  setTimeout(() => {
     copyMsg.classList.remove("active");
-  },2000);
-
-
+  }, 2000);
 }
 
+function shuffelPassword(array) {
+  //Fisher Yates Method
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    const temp = array[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+  let str = "";
+  array.forEach((el) => (str += el));
+  return str;
+}
 
-function shuffelPassword(array){
-    //Fisher Yates Method 
-    for(let i=array.length-1;i>0;i--){
-        const j=Math.floor(Math.random()*(i+1));
-        const temp=array[i];
-        array[i]=array[j];
-        array[j]=temp;
+function handleCheckBoxChange() {
+  checkCount = 0;
+  allCheckBox.forEach((checkbox) => {
+    if (checkbox.checked) {
+      checkCount++;
     }
-    let str="";
-    array.forEach((el)=>(str+=el));
-    return str;
-}
+  });
 
-function handleCheckBoxChange(){
-    checkCount=0;
-    allCheckBox.forEach((checkbox)=>{
-        if(checkbox.checked){
-            checkCount++;
-        }
-    });
-
-    //special case
-    if(passwordLength<checkCount){
-        passwordLength=checkCount;
-        handleSlider();
-    }
-}
-    allCheckBox.forEach((checkbox)=>{
-        checkbox.addEventListener('change',handleCheckBoxChange);
-    })
-
-
-inputSlider.addEventListener('input',(e)=>{
-    passwordLength=e.target.value;
+  //special case
+  if (passwordLength < checkCount) {
+    passwordLength = checkCount;
     handleSlider();
-})
+  }
+}
+allCheckBox.forEach((checkbox) => {
+  checkbox.addEventListener("change", handleCheckBoxChange);
+});
 
-copyBtn.addEventListener('click',(e)=>{
-    if(passwordDisplay.value){
-        copyContent();
-    }
-})
+inputSlider.addEventListener("input", (e) => {
+  passwordLength = e.target.value;
+  handleSlider();
+});
 
-generateBtn.addEventListener('click',(e)=>{
-    //none of the checkbox are selected
-    if(checkCount==0) {
-        return;}
-    if(passwordLength<checkCount){
-        passwordLength=checkCount;
-        handleSlider();
-    }
+copyBtn.addEventListener("click", (e) => {
+  if (passwordDisplay.value) {
+    copyContent();
+  }
+});
 
-    //lets start the journey to find new password
-    console.log("Start journay");
-    //remove old password
-    password="";
+generateBtn.addEventListener("click", (e) => {
+  //none of the checkbox are selected
+  if (checkCount == 0) {
+    return;
+  }
+  if (passwordLength < checkCount) {
+    passwordLength = checkCount;
+    handleSlider();
+  }
 
-    //lets put the stuff mentioned by checkboxes
-    // if(uppercaseCheck.checked){
-    //     password+=generateUpperCase();
-    // }
-    // if(lowercaseCheck.checked){
-    //     password+=generateLowerCase();
-    // }
-    // if(numbersCheck.checked){
-    //     password+=generateRandomNumber();
-    // }
-    // if(symbolsCheck.checked){
-    //     password+=generateSymbol();
-    // }
+  //lets start the journey to find new password
+  console.log("Start journay");
+  //remove old password
+  password = "";
 
-    let funArr=[];
+  //lets put the stuff mentioned by checkboxes
+  // if(uppercaseCheck.checked){
+  //     password+=generateUpperCase();
+  // }
+  // if(lowercaseCheck.checked){
+  //     password+=generateLowerCase();
+  // }
+  // if(numbersCheck.checked){
+  //     password+=generateRandomNumber();
+  // }
+  // if(symbolsCheck.checked){
+  //     password+=generateSymbol();
+  // }
 
-    if(uppercaseCheck.checked){
-        funArr.push(generateUpperCase);
-    }
-    if(lowercaseCheck.checked){
-        funArr.push(generateLowerCase);
-    }
-    if(numbersCheck.checked){
-        funArr.push(generateRandomNumber);
-    }
-    if(symbolsCheck.checked){
-        funArr.push(generateSymbol);
-    }
+  let funArr = [];
 
-    //compuldary addition
-    for(let i=0;i<funArr.length;i++){
-        password += funArr[i]();
-    }
-    console.log("Addition done");
-    //remaining addition
-    for(let i=0;i<passwordLength-funArr.length;i++ ){
-        let randIndex=getRndInteger(0,funArr.length);
-        password += funArr[randIndex]();
-    }
-    console.log("Remaining addition done");
-    //shuffel the password
-    password=shuffelPassword(Array.from(password));
-    console.log("Shuffling done");
+  if (uppercaseCheck.checked) {
+    funArr.push(generateUpperCase);
+  }
+  if (lowercaseCheck.checked) {
+    funArr.push(generateLowerCase);
+  }
+  if (numbersCheck.checked) {
+    funArr.push(generateRandomNumber);
+  }
+  if (symbolsCheck.checked) {
+    funArr.push(generateSymbol);
+  }
 
-    //show in UI
-    passwordDisplay.value=password;
-console.log("UI addition done");
-    //calculate strength
-    calcStrength();
-})
+  //compuldary addition
+  for (let i = 0; i < funArr.length; i++) {
+    password += funArr[i]();
+  }
+  console.log("Addition done");
+  //remaining addition
+  for (let i = 0; i < passwordLength - funArr.length; i++) {
+    let randIndex = getRndInteger(0, funArr.length);
+    password += funArr[randIndex]();
+  }
+  console.log("Remaining addition done");
+  //shuffel the password
+  password = shuffelPassword(Array.from(password));
+  console.log("Shuffling done");
+
+  //show in UI
+  passwordDisplay.value = password;
+  console.log("UI addition done");
+  //calculate strength
+  calcStrength();
+});
